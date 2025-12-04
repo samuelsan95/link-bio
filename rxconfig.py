eb5e511a4d6aa48b77cf4e61dd03c9ef0691ec5f
Samuel Sanchez <sanchezlopezsamuelgmail.com>
2025-12-04 18:15:14 +0100

    Fix: Remove explicit API URL export from deploy workflow and add  configuration

--- rxconfig.py
+++ rxconfig.py
@@ -4,5 +4,6 @@ config = rx.Config(
     app_name="link_bio",
     tailwind=None,
     show_built_with_reflex=False,
-    disable_plugins=['reflex.plugins.sitemap.SitemapPlugin']
+    disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
+    _no_sio=True
 )
